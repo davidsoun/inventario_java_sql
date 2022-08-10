24718e2068f1b01e14c9ca3fcd9530e4721c4d29
David Sosa <106178899+davidsoun@users.noreply.github.com>
2022-08-10 09:31:00 -0500

    Add files via upload

---
+++ inventario_labcel/src/proyecto_ingsoft/Proyecto_ingsoft.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_ingsoft;
+
+import formulario.frm_menu;
+
+/**
+ *
+ * @author Liches
+ */
+public class Proyecto_ingsoft {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_menu.main(args);
+    }
+    
+}
